5c880706ee07416c7714b65892f0c6e66dec86cc
Pablo Collins <pcollins@splunk.com>
2025-03-07 11:58:50 -0500

    Move setup tracing and logging into OtelMiniManager

--- src/otelmini/auto/__init__.py
+++ src/otelmini/auto/__init__.py
@@ -28,39 +28,47 @@ class OtelMiniManager:
             root_logger = logging.getLogger()
             root_logger.removeHandler(self.otel_handler)
 
+    def set_up_tracing(self):
+        """Set up OpenTelemetry tracing."""
+        tracer_provider = TracerProvider(
+            BatchProcessor(
+                GrpcSpanExporter(),
+                batch_size=144,
+                interval_seconds=12,
+            )
+        )
+        trace.set_tracer_provider(tracer_provider)
+        self.tracer_provider = tracer_provider
+
+    def set_up_logging(self, exporter=None):
+        """Set up OpenTelemetry logging."""
+        root_logger = logging.getLogger()
+        if exporter is None:
+            exporter = GrpcLogExporter()
+        
+        logger_provider = LoggerProvider([BatchLogRecordProcessor(exporter)])
+        otel_handler = OtelBridgeHandler(logger_provider)
+        
+        root_logger.addHandler(otel_handler)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+        root_logger.addHandler(stream_handler)
+
+        # Store references for shutdown
+        self.logger_provider = logger_provider
+        self.otel_handler = otel_handler
+
 
 # Global instance to track OpenTelemetry components
 manager = OtelMiniManager()
 
-
+# Convenience functions that delegate to the global manager
 def set_up_tracing():
     """Set up OpenTelemetry tracing."""
-    tracer_provider = TracerProvider(
-        BatchProcessor(
-            GrpcSpanExporter(),
-            batch_size=144,
-            interval_seconds=12,
-        )
-    )
-    trace.set_tracer_provider(tracer_provider)
-    manager.tracer_provider = tracer_provider
+    manager.set_up_tracing()
 
 
 def set_up_logging(exporter=None):
     """Set up OpenTelemetry logging."""
-    root_logger = logging.getLogger()
-    if exporter is None:
-        exporter = GrpcLogExporter()
-    
-    logger_provider = LoggerProvider([BatchLogRecordProcessor(exporter)])
-    otel_handler = OtelBridgeHandler(logger_provider)
-    
-    root_logger.addHandler(otel_handler)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
-    root_logger.addHandler(stream_handler)
-
-    # Store references for shutdown
-    manager.logger_provider = logger_provider
-    manager.otel_handler = otel_handler
+    manager.set_up_logging(exporter)
