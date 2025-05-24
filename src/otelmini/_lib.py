77d14dba869eb25aa18533458a55bf38ad240d01
Pablo Collins <pabcolli@cisco.com>
2025-05-23 20:47:24 -0700

    Add abort_retry field to ExponentialBackoff

--- src/otelmini/_lib.py
+++ src/otelmini/_lib.py
@@ -6,6 +6,8 @@ from abc import ABC, abstractmethod
 from enum import Enum
 from typing import Generic, Sequence, TypeVar
 
+from grpc import StatusCode
+
 T = TypeVar("T")
 _pylogger = logging.getLogger(__package__)
 
@@ -22,11 +24,12 @@ class ExportResult(Enum):
 
 
 class ExponentialBackoff:
-    def __init__(self, max_retries, base_seconds=1, sleep=time.sleep, exceptions=(Exception,)):
+    def __init__(self, max_retries, base_seconds=1, sleep=time.sleep, exceptions=(Exception,), abort_retry=lambda _: False):
         self.max_retries = max_retries
         self.base_seconds = base_seconds
         self.sleep = sleep
         self.exceptions = exceptions
+        self.abort_retry = abort_retry
 
     def retry(self, func):
         for attempt in range(self.max_retries + 1):
@@ -34,6 +37,8 @@ class ExponentialBackoff:
             try:
                 return func()
             except self.exceptions as e:
+                if self.abort_retry(e):
+                    return None
                 if attempt < self.max_retries:
                     seconds = (2 ** attempt) * self.base_seconds
                     _pylogger.warning("Retry will sleep %d seconds", seconds)
@@ -46,3 +51,15 @@ class ExponentialBackoff:
         def __init__(self, last_exception):
             super().__init__("Maximum retries reached")
             self.last_exception = last_exception
+
+
+def _is_retryable(status_code: StatusCode):
+    return status_code in [
+        StatusCode.CANCELLED,
+        StatusCode.DEADLINE_EXCEEDED,
+        StatusCode.RESOURCE_EXHAUSTED,
+        StatusCode.ABORTED,
+        StatusCode.OUT_OF_RANGE,
+        StatusCode.UNAVAILABLE,
+        StatusCode.DATA_LOSS,
+    ]
--- tests/test_trace.py
+++ tests/test_trace.py
@@ -3,7 +3,7 @@ import pickle
 
 import pytest
 from _lib import mk_span
-from grpc import RpcError
+from grpc import RpcError, StatusCode
 from opentelemetry.proto.collector.trace.v1.trace_service_pb2 import (
     ExportTraceServiceRequest,
     ExportTraceServiceResponse,
@@ -15,26 +15,39 @@ from otelmini.processor import Timer
 from otelmini.trace import GrpcSpanExporter, MiniSpan, Resource, InstrumentationScope, SpanContext
 
 
-def test_eventual_runner():
-    runner = EventualRunner(1, lambda: "hello")
+def test_stubborn_runner():
+    runner = StubbornRunner(1, lambda: "hello")
     with pytest.raises(Exception):
         runner.attempt()
     assert runner.attempt() == "hello"
 
 
-def test_retrier_eventual_success():
-    greeter = EventualRunner(2, lambda: "hello")
+def test_backoff_eventual_success():
+    greeter = StubbornRunner(2, lambda: "hello")
     f = FakeSleeper()
-    retrier = ExponentialBackoff(max_retries=2, sleep=f.sleep)
-    assert retrier.retry(lambda: greeter.attempt()) == "hello"
+    backoff = ExponentialBackoff(max_retries=2, sleep=f.sleep)
+    assert backoff.retry(lambda: greeter.attempt()) == "hello"
     assert f.sleeps == [1, 2]
 
 
-def test_retrier_eventual_failure():
-    retrier = ExponentialBackoff(max_retries=1, sleep=FakeSleeper().sleep)
+def test_backoff_eventual_failure():
+    backoff = ExponentialBackoff(max_retries=1, sleep=FakeSleeper().sleep)
     with pytest.raises(ExponentialBackoff.MaxAttemptsError):
-        greeter = EventualRunner(2, lambda: "hello")
-        retrier.retry(lambda: greeter.attempt())
+        greeter = StubbornRunner(2, lambda: "hello")
+        backoff.retry(lambda: greeter.attempt())
+
+
+def test_backoff_abort_retry():
+    def abort_retry(e: RpcError):
+        if hasattr(e, "code") and e.code:
+            return e.code() != StatusCode.UNAVAILABLE
+        return True
+
+    def my_function():
+        raise RpcError()
+
+    backoff = ExponentialBackoff(max_retries=1, sleep=FakeSleeper().sleep, abort_retry=abort_retry)
+    backoff.retry(my_function)
 
 
 def test_faked_exporter_with_retry_then_success():
@@ -80,10 +93,10 @@ def test_span_dict_serialization():
         instrumentation_scope=InstrumentationScope("", ""),
         on_end_callback=lambda s: None
     )
-    
+
     span_dict = span.to_dict()
     new_span = MiniSpan.from_dict(span_dict, on_end_callback=lambda s: None)
-    
+
     assert new_span.get_name() == "test"
     assert new_span.get_span_context().trace_id == 1
     assert new_span.get_span_context().span_id == 2
@@ -119,7 +132,7 @@ class FakeSleeper:
         self.sleeps.append(seconds)
 
 
-class EventualRunner:
+class StubbornRunner:
     """For testing Retrier"""
 
     def __init__(self, num_failures_before_success, func, exception=Exception()):
